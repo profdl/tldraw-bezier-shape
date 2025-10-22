066b8deb2ffdefe3c18d3bd9d991986fc3882a8a
profdl <112704467+profdl@users.noreply.github.com>
2025-10-22 09:31:19 -0400

    Removed legacy code

--- src/components/shapes/utils/bezierPathHelpers.ts
+++ src/components/shapes/utils/bezierPathHelpers.ts
@@ -2,51 +2,6 @@ import type { BezierPoint } from '../BezierShape'
 import { BezierBounds } from '../services/BezierBounds'
 
 /**
- * Convert BezierPoint array to SVG path data.
- */
-export function bezierPointsToPath(points: BezierPoint[], isClosed: boolean): string {
-  if (points.length === 0) return ''
-
-  const commands: string[] = []
-  const firstPoint = points[0]
-  commands.push(`M ${firstPoint.x} ${firstPoint.y}`)
-
-  for (let i = 1; i < points.length; i++) {
-    const prevPoint = points[i - 1]
-    const currPoint = points[i]
-
-    if (prevPoint.cp2 && currPoint.cp1) {
-      commands.push(`C ${prevPoint.cp2.x} ${prevPoint.cp2.y} ${currPoint.cp1.x} ${currPoint.cp1.y} ${currPoint.x} ${currPoint.y}`)
-    } else if (prevPoint.cp2) {
-      commands.push(`Q ${prevPoint.cp2.x} ${prevPoint.cp2.y} ${currPoint.x} ${currPoint.y}`)
-    } else if (currPoint.cp1) {
-      commands.push(`Q ${currPoint.cp1.x} ${currPoint.cp1.y} ${currPoint.x} ${currPoint.y}`)
-    } else {
-      commands.push(`L ${currPoint.x} ${currPoint.y}`)
-    }
-  }
-
-  if (isClosed && points.length > 2) {
-    const lastPoint = points[points.length - 1]
-    const first = points[0]
-
-    // Draw the closing segment from last point back to first point
-    if (lastPoint.cp2 && first.cp1) {
-      commands.push(`C ${lastPoint.cp2.x} ${lastPoint.cp2.y} ${first.cp1.x} ${first.cp1.y} ${first.x} ${first.y}`)
-    } else if (lastPoint.cp2) {
-      commands.push(`Q ${lastPoint.cp2.x} ${lastPoint.cp2.y} ${first.x} ${first.y}`)
-    } else if (first.cp1) {
-      commands.push(`Q ${first.cp1.x} ${first.cp1.y} ${first.x} ${first.y}`)
-    } else {
-      commands.push(`L ${first.x} ${first.y}`)
-    }
-    commands.push('Z')
-  }
-
-  return commands.join(' ')
-}
-
-/**
  * Convert a single segment to SVG path data for highlighting / selection visuals.
  */
 export function bezierSegmentToPath(points: BezierPoint[], segmentIndex: number, isClosed: boolean): string {
